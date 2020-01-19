7f2cdcc2d0d93cd628148d144a4ce92f3f28142a
David Alexys <davider98@hotmail.com>
2020-01-18 19:51:26 -0700

    Initial Commit

---
+++ Hamburguesas/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hamburguesas
+//
+//  Created by David Garcia hernandez on 18/01/20.
+//  Copyright © 2020 David Garcia hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
