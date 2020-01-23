861a49cfd15c204a2ee99b4a9d6a7c61b9e822fc
David Alexys <davider98@hotmail.com>
2020-01-22 17:19:23 -0700

    Initial Commit

---
+++ Pizza assembly/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pizza assembly
+//
+//  Created by David Garcia hernandez on 22/01/20.
+//  Copyright © 2020 David Garcia hernandez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
