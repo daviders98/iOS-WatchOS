7dc588b766dd3431727e5bf842ee75c22ca9a13d
David Alexys <davider98@hotmail.com>
2020-02-15 21:14:24 -0700

    zx

--- Book requests/ViewController.swift
+++ Book requests/ViewController.swift
@@ -8,13 +8,37 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UISearchBarDelegate{
+    var solicitud = ""
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Busqueda.delegate = self
     }
-
+    
+    @IBOutlet weak var Busqueda: UISearchBar!
+    
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        self.Busqueda.endEditing(true)
+        if Busqueda.text != nil{
+            solicitud = Busqueda.text!
+            let url = NSURL(string: Request())
+            let datos : NSData? = NSData(contentsOf: url! as URL)
+            if datos == nil{
+                Resultado.text = "ERROR, no hay conexión a internet"
+            }else{
+                let text = NSString(data: datos! as Data, encoding: String.Encoding.utf8.rawValue)
+                Resultado.text = String(text!)
+            }
+        }
+    }
+    
+    func Request()-> String{
+       let str = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        return str + "\(solicitud)"
+    }
+    @IBOutlet weak var Resultado: UITextView!
+    
 
 }
 
